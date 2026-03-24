4ac0824d03c3dc5295ecd94c2503c9437139b848
JohnnyMoney <wyndrockjohn@gmail.com>
2026-03-24 18:35:45 -0400

    Delete src directory

--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-export function setbuttonText(btn, isloading, loadingText = "Saving...", defaultText = "Save") {
-  if (isloading) {
-    btn.textContent = loadingText;
-  } else {
-    btn.textContent = defaultText;
-  }
-}
-
-
